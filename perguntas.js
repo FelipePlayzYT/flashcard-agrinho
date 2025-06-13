f158bc29206836fd11a229978b8ac4ecf5585911
FelipePlayzYT <54115676+FelipePlayzYT@users.noreply.github.com>
2025-06-13 09:37:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Onde fica a mata atlântica?',
+    'Fica no Brasil.'
+)
+
+criaCartao(
+    'Geografia',
+    'Por que tem muito desmatamento nas florestas brasileiras?',
+    'Por causa da expansão da agropecuaria em areas privadas.'
+)
+
+criaCartao(
+    'Historia',
+    'Quem foi Pedro Alvares Cabral?',
+    'A pessoa que descobriu o Brasil.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Adeus em Inglês?',
+    'Adeus em ingles é BYE (BAIEH)'
+)
